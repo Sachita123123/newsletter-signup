d83119870d8396848f572cdb3fc86d9184c6853a
Sachita <sachitak123123@gmail.com>
2022-10-31 00:59:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const https = require("node:https");
+const bodyParser = require("body-parser")
+const client = require("@mailchimp/mailchimp_marketing");
+
+
+const app = express();
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+
+
+
+
+app.post("/" ,function(req,res) {
+
+    
+    const listId = "02568d9a43"
+
+    var firstName = req.body.first
+    var lastName = req.body.last
+    var email = req.body.email
+    
+
+
+
+    client.setConfig({
+        apiKey: "cb8defcfec43d2fd414ec8a06d80b788-us10",
+        server: "us10"
+        
+      });
+      const run = async () => {
+        const response = await client.lists.batchListMembers(listId, {
+            members: [{
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }],
+        });
+        
+        if (response.error_count ==0){
+            res.sendFile(__dirname+"/success.html")
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+      };
+
+      run();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+
+})
+
+
+
+
+
+app.listen(process.env.PORT||3000, function(){
+    console.log("Server is runing ");
+});
